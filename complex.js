735cc653a7b4e22fc53cec5d23be681b084d0ea3
marcinfilipowicz <73066958+marcinfilipowicz@users.noreply.github.com>
2020-12-15 20:36:03 +0100

    Create complex.js

---
+++ complex.js
@@ -0,0 +1,27 @@
+"use strict";
+var Complex = /** @class */ (function () {
+    function Complex(re, im) {
+        this.re = re;
+        this.im = im;
+    }
+    Complex.prototype.add = function (com) {
+        var a = this.re + com.re;
+        var b = this.im + com.im;
+        return new Complex(a, b);
+    };
+    Complex.prototype.subtract = function (com) {
+        var a = this.re - com.re;
+        var b = this.im - com.im;
+        return new Complex(a, b);
+    };
+    Complex.prototype.toString = function () {
+        console.log("re: ", this.re, ", ", "im: ", this.im);
+    };
+    return Complex;
+}());
+var a = new Complex(3, 4);
+var b = new Complex(7, 4);
+var c = a.add(b);
+var d = new Complex(3, -2);
+var e = c.subtract(d);
+e.toString();
